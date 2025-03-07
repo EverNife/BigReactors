a9784d65c832cfd5c8a0c3059d51a64c15e40dae
Pétrus Pradella <petrus_ant@hotmail.com>
2025-03-07 09:27:50 -0300

    Fix versions tag

--- src/main/java/erogenousbeef/bigreactors/common/BRConfig.java
+++ src/main/java/erogenousbeef/bigreactors/common/BRConfig.java
@@ -2,6 +2,7 @@ package erogenousbeef.bigreactors.common;
 
 import java.io.File;
 
+import erogenousbeef.bigreactors.Tags;
 import net.minecraftforge.common.config.Configuration;
 import cpw.mods.fml.common.Loader;
 
@@ -10,7 +11,7 @@ public class BRConfig {
 	 * The version of Big Reactors.
 	 * These are replaced by Gradle when built.
 	 */
-	public static final String VERSION = "@VERSION@";
+	public static final String VERSION = Tags.VERSION;
 	public static final String MINECRAFT_VERSION = "[1.7.10]";
 
 	public static final int WORLDGEN_VERSION = 1; // Bump this when changing world generation so the world regens
